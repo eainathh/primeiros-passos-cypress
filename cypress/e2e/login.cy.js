29f25a9e13877eecb043e864ebb707cb3c2b7c04
eainathh <nathaliagsm02@gmail.com>
2024-11-28 21:40:18 -0300

    Base Url added

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,21 +6,22 @@ describe('Orange HRM Testes', () => {
     usernameField: "[name='username']",
     passwordField: "[name='password']",
     loginButton: "[type='submit']",
+    dashboardGrid: ".orangehrm-dashboard-grid",
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
     wrongCredentialsAlert: "[role='alert']"
   }
 
   
   it('Login - Sucess', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click();
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index');
-    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard');
+    cy.get(selectorsList.dashboardGrid)
   })
   it('Login - Fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
     cy.get(selectorsList.passwordField).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
